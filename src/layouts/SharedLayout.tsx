99c60024e561c805b2202a9f667d5fa0a798afca
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-05-06 16:48:33 +0300

    added useReducer

--- src/layouts/SharedLayout.tsx
+++ src/layouts/SharedLayout.tsx
@@ -1,10 +1,127 @@
 import { Outlet } from 'react-router-dom';
 import TheHeader from "../components/TheHeader/TheHeader";
+import {useEffect, useState, useReducer, Reducer} from "react";
+import axios from 'axios';
+
+// enum UsersActions {
+//     ADD_USER = 'ADD_USER',
+//     SET_USERS = 'SET_USERS',
+// }
+//
+// interface ICompany {
+//     name: string;
+//     catchPhrase: string;
+//     bs: string;
+// }
+//
+// interface IAddress {
+//     suite: string;
+//     city: string;
+//     zipcode: string;
+//     geo: any;
+// }
+//
+// interface IUser {
+//     id: number;
+//     website: string;
+//     username: string;
+//     phone: string;
+//     name: string;
+//     email: string;
+//     company: ICompany;
+//     address: IAddress;
+// }
+//
+// interface IPropInitialState {
+//     usersList: IUser[] | [];
+// }
+//
+// interface UserAction {
+//     type: UsersActions;
+//     payload: string;
+// }
+//
+// interface IssueReducer {
+//     state: IPropInitialState;
+//     action: {
+//         type: UsersActions;
+//         payload: string;
+//     };
+// }
+//
+// const initialStateTest = {
+//     usersList: []
+// }
+
+// const reducer: Reducer<any, any> = (state = [], action) => {
+//     // const { type, payload } = action;
+//     switch (action.type) {
+//         case UsersActions.ADD_USER: {
+//             let usersList = state.usersList;
+//             usersList.push(action.payload)
+//             return {
+//                 ...state.usersList,
+//                 usersList,
+//             }
+//         }
+//         case UsersActions.SET_USERS: {
+//             console.log('reducer', state);
+//             console.log('action.payload', action.payload);
+//             return {
+//                 usersList: [...state.usersList, action.payload]
+//             }
+//         }
+//         default:
+//             return state;
+//     }
+// }
+//
+// const initialState = {
+//     usersList: [],
+// }
 
 function SharedLayout(): JSX.Element {
+
+    const [users, setUsers] = useState<any[]>([]);
+    // const [state, dispatch] = useReducer(reducer, {usersList: []});
+    const [userName, setUserName] = useState<string>('');
+
+    const changeValue = (event: any): void => {
+        setUserName(event.target.value)
+    }
+
+    const addNewUser = () => {
+        console.log('addNewUser');
+    }
+
+
+    useEffect(() => {
+        // console.log('state', state);
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                // const persons = ['1','2','3','4','5'];
+                // setUsers(persons);
+                // dispatch({
+                //     type: UsersActions.SET_USERS,
+                //     payload: persons,
+                // })
+                // console.log('state', state);
+            });
+        console.log('useEffect')
+    }, []);
+
     return (
         <>
             <TheHeader />
+            <input type="text" value={userName} onChange={changeValue}/>
+            <button onClick={addNewUser}>Add new</button>
+            {
+                users.map((item: any) => (
+                    <div key={item}>
+                        {item}
+                    </div>
+                ))
+            }
             <section className='section'>
                 <Outlet />
             </section>
